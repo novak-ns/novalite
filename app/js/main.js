a8548bbaa02aa09f8fe80ddf640898c1b7ab7fa6
novak-ns <105936452+novak-ns@users.noreply.github.com>
2023-03-14 15:42:57 +0100

    responsive, not complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,21 @@
+const hamburger = document.getElementById('hamburger');
+const hamburgerspan = document.getElementsByClassName("hamburger_span");
+const navLinks = document.getElementById('nav');
+const body = document.getElementById('body');
 
+        
+        document.onclick = function(e){
+            if(e.target.id !=='hamburger' && e.target.className !=="hamburger_span")
+            {
+                hamburger.classList.remove('open')
+                navLinks.classList.remove('active')
+                body.classList.remove("fixed-class")
+            }
+        }
+        
+        hamburger.onclick = function(){
+            hamburger.classList.toggle('open')
+            navLinks.classList.toggle('active')
+            body.classList.toggle("fixed-class")
+        }
 
-      /* $(document).ready(function(){
-
-        $('.icon').on('click', function(e) {
-          e.preventDefault();
-          $(this).closest('.single-item').find('.tooltip-content').addClass("open");
-          console.log("test");
-           $('body').addClass("fixed-class");
-           $('body').addClass("overlay");
-        });
-    
-         $('.tooltip').on('click', function(e) {
-           e.preventDefault();
-           $(this).closest('.single-item').find('.tooltip-content').addClass("open");
-           $('body').addClass("fixed-class");
-           $('body').addClass("overlay");
-         });
-    
-         $('.exit').on('click', function(e) {
-           e.preventDefault();
-           $('.tooltip-content').removeClass("open");
-           $('body').removeClass("fixed-class");
-           $('body').removeClass("overlay");
-         });
-      });  /*
